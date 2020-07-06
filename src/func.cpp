5894df5dffc1167a69c60f04f1549c610edc8c93
Caroline Lemieux <clemieux@cs.berkeley.edu>
2020-07-06 10:57:43 -0700

    Bugfix....

--- src/func.cpp
+++ src/func.cpp
@@ -1,12 +1,13 @@
+#include <iostream>
 #include "include/func.hpp"
 
 int parse(std::string parsable) {
    if (parsable.size() < 4) {
      return 0;
    }
-   if (parsable.compare(0, 3, "GIF")){
+   if (parsable.compare(0, 3, "GIF") == 0){
     return 2;
-   } else if (parsable.compare(0,3, "JPG")) {
+   } else if (parsable.compare(0,3, "JPG") == 0) {
     return 1;
    }
    return 0;
